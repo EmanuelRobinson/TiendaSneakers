881e87d993ee1cd7c4bc7548c9a4dd9a2ea6de5e
Emaa Dev <emarobins.oh@gmail.com>
2025-03-05 23:07:21 -0500

    Revert "cambios"
    This reverts commit 7320abf6485c76fdd2a91851c14676fe38a25a92.

--- App/Views/Caballeros.php
+++
@@ -1,4 +0,0 @@
-<div class="contenido">
-    <h2>Sección de Caballeros</h2>
-    <p>Aquí puedes ver los productos para caballeros.</p>
-</div>
--- App/Views/nenes.php
+++
@@ -1,4 +0,0 @@
-<div class="contenido">
-    <h2>Sección de Niños</h2>
-    <p>Aquí puedes ver los productos para niños.</p>
-</div>
