57f581e45276b7e2968a6ff9e5517ab63218f7af
Emaa Dev <emarobins.oh@gmail.com>
2025-03-07 12:45:23 -0500

    modificacion

--- App/Views/Damas.php
+++
@@ -1,4 +0,0 @@
-<div class="contenido">
-    <h2>Sección de Damas</h2>
-    <p>Aquí puedes ver los productos para damas.</p>
-</div>
--- App/Views/Inicio.php
+++
@@ -1,5 +0,0 @@
-<div class="contenido">
-    <h2>Página de Inicio</h2>
-    <p>Bienvenido a nuestra tienda online. Explora nuestras categorías.</p>
-    <img src="../../Public/Images/hojas.jfif" alt="Imagen decorativa">
-</div>
